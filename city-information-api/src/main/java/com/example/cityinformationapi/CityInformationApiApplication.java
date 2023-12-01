e6e769fd37bdc24d2b8c1c09b72cec3530c4dcb0
Nikola Get <nikola.get@gmail.com>
2023-12-01 11:56:53 +0100

    Initial commit

---
+++ city-information-api/src/main/java/com/example/cityinformationapi/CityInformationApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.cityinformationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityInformationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityInformationApiApplication.class, args);
+	}
+
+}
---
+++ city-information-api/src/test/java/com/example/cityinformationapi/CityInformationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cityinformationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityInformationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
